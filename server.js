c4b018724fc68cb89333497b9d2203317bbbeb25
Kevin <Kevin@Kevins-MacBook-Air.local>
2013-11-03 00:33:21 -0400

    fixed server file for heroku and ignored .DS_Store files

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.use(express.static(__dirname + '/src'));
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/src'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
